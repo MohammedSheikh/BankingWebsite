882d82be168f57623935e7b9a43d8d492d9718bc
mohammdsheikh <p14165162@myemail.dmu.ac.uk>
2017-03-19 18:51:08 +0000

    Added getter and setter to 'Employee' class

--- App_Code/Customer.cs
+++ App_Code/Customer.cs
@@ -11,7 +11,7 @@ public class Customer:Person{
 
    
 
-    public Customer(string firstName1, string lastName1, string addressOne1, string addressTwo1, string addressThree1, string city1, string postCode1, string county1, string country1, char mobileNumber1, char homeNumber1, string email1, DateTime DOB1, DateTime joinedDate1) : base(firstName1, lastName1, addressOne1, addressTwo1, addressThree1, city1, postCode1, county1, country1, mobileNumber1, homeNumber1, email1, DOB1)
+    public Customer(string firstName1, string lastName1, string addressOne1, string addressTwo1, string addressThree1, string city1, string postCode1, string county1, string country1, char mobileNumber1, char homeNumber1, string email1, DateTime dOB1, DateTime joinedDate1) : base(firstName1, lastName1, addressOne1, addressTwo1, addressThree1, city1, postCode1, county1, country1, mobileNumber1, homeNumber1, email1, dOB1)
     {
         joinedDate1 = joinedDate;
     }
--- App_Code/Employee.cs
+++ App_Code/Employee.cs
@@ -6,8 +6,24 @@ using System.Web;
 
 public class Employee : Person{
 
-    
-    public Employee(string firstName1, string lastName1, string addressOne1, string addressTwo1, string addressThree1, string city1, string postCode1, string county1, string country1, char mobileNumber1, char homeNumber1, string email1, DateTime DOB1) : base(firstName1, lastName1, addressOne1, addressTwo1, addressThree1, city1, postCode1, county1, country1, mobileNumber1, homeNumber1, email1, DOB1)
+
+    private static DateTime employeeJoinedDate;
+
+    public Employee(string firstName1, string lastName1, string addressOne1, string addressTwo1, string addressThree1, string city1, string postCode1, string county1, string country1, char mobileNumber1, char homeNumber1, string email1, DateTime dOB1, DateTime employeeJoinedDate1) : base(firstName1, lastName1, addressOne1, addressTwo1, addressThree1, city1, postCode1, county1, country1, mobileNumber1, homeNumber1, email1, dOB1)
     {
+        employeeJoinedDate1 = employeeJoinedDate;
     }
+        public static DateTime EmployeejoinedDate
+    {
+        get
+        {
+            return employeeJoinedDate;
+        }
+
+        set
+        {
+            employeeJoinedDate = value;
+        }
+    }
+
 }
