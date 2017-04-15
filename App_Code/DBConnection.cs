35fef115b98d176d233b876d2c1f492ad2901027
mohammdsheikh <p14165162@myemail.dmu.ac.uk>
2017-04-15 22:25:52 +0100

    Added database connection class + modified connection strings

---
+++ App_Code/DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for DBConnection
+/// </summary>
+public class DBConnection
+{
+    private static string connectionString = ConfigurationManager.ConnectionStrings["Banking"].ConnectionString;
+
+    public static string ConnectionString
+    {
+        get
+        {
+            return connectionString;
+        }
+        set
+        {
+            connectionString = value;
+        }
+    }
+}
