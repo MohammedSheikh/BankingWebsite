daea3608887919e7c558416a5ab07eb4f8b4902f
Alam <aahmed9510@gmail.com>
2017-04-29 15:29:08 +0100

    I have made a very big mistake - this is not working but Mo is telling me to push -- hard

--- MyDetails.aspx.cs
+++ MyDetails.aspx.cs
@@ -9,7 +9,56 @@ public partial class MyDetails : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+    
+        Address1.Text =("Ram");
+        Address2.Text = ("Ram");
+        Address3.Text = ("Ram");
+        PostCode.Text = ("Ram");
+        homePhone.Text = ("Ram");
+        mobile.Text = ("Ram");
+        email.Text = ("Ram");
         
+    }
+
+    protected void textCheck(object sender, EventArgs e)
+    {
+        saveChangesButton.Enabled = String.IsNullOrEmpty(Address1.Text);
+
+    }
+
+    protected void textCheck2(object sender, EventArgs e)
+    {
+        saveChangesButton.Enabled = !String.IsNullOrEmpty(Address2.Text);
+
+    }
+
+    protected void textCheck3(object sender, EventArgs e)
+    {
+        saveChangesButton.Enabled = !String.IsNullOrEmpty(Address3.Text);
+
+    }
+
+    protected void textCheck4(object sender, EventArgs e)
+    {
+        saveChangesButton.Enabled = !String.IsNullOrEmpty(PostCode.Text);
+
+    }
+
+    protected void textCheck5(object sender, EventArgs e)
+    {
+        saveChangesButton.Enabled = !String.IsNullOrEmpty(homePhone.Text);
+
+    }
+
+    protected void textCheck6(object sender, EventArgs e)
+    {
+        saveChangesButton.Enabled = !String.IsNullOrEmpty(mobile.Text);
+
+    }
+
+    protected void textCheck7(object sender, EventArgs e)
+    {
+        saveChangesButton.Enabled = !String.IsNullOrEmpty(email.Text);
 
     }
 }
