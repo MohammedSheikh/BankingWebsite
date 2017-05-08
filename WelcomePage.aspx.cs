de7740dd0caee7a51d81bf25bd42a72df8b908ee
Alam <aahmed9510@gmail.com>
2017-05-08 21:38:20 +0100

    After two painful weeks we finally have the connection string working for the SELECT query. Credit to the team! (L, M & Y)

--- WelcomePage.aspx.cs
+++ WelcomePage.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,45 +11,40 @@ using System.Xml;
 
 public partial class WelcomePage : System.Web.UI.Page
 {
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        // Label1.Text = Session["id"].ToString() + "!";
-//The code below redirects to the login page if they user has logged out and tried to access the welcome page
-        //if (Session["id"] == null)
-        //{
-        //    Response.Redirect("LoginPage.aspx");
-        //}
-        //else
-        //{
-            //Label1.Text = DataTable.Rows[0]["FirstName"];
-            SqlConnection sqlConnection1 = new SqlConnection(DBConnection.ConnectionString);
-            //SqlCommand cmd = new SqlCommand();
-            // SqlDataReader reader;
-            sqlConnection1.Open();
 
-            //cmd.CommandText = "SELECT FirstName FROM Customer where CustomerID = " + Session["id"];
-            //cmd.CommandType = CommandType.Text;
-            //cmd.Connection = sqlConnection1;
+        string name = null;
+        string department = null;
+        Boolean j = true;
 
-            string result = "SELECT FirstName FROM Customer where CustomerID = " + Session["id"];
-            SqlCommand showresult = new SqlCommand(result);
-            
-            //Label1.Text = showresult.ExecuteScalar().ToString();
-
-        var firstColumn = showresult.ExecuteScalar();
-
-        if (firstColumn != null)
+        string sql = "SELECT FirstName FROM Customer where CustomerID =" + Session["id"];
+        //change this connection string... visit www.connectionstrings.com
+        string connString = DBConnection.ConnectionString;
+        using (SqlConnection conn = new SqlConnection(connString))
         {
-            Label1.Text = firstColumn.ToString();
+            conn.Open();
+            using (SqlCommand command = new SqlCommand(sql, conn))
+            {
+                SqlDataReader reader = command.ExecuteReader();
+                while (j == true)
+                {
+                    reader.Read();
+                    name = reader[0] as string;
+                    //break for single row or you can continue if you have multiple rows...
+                    Label1.Text = name + " tst " + department;
+                    break;
+
+                }
+            }
+            conn.Close();
         }
 
-        //reader = cmd.ExecuteReader();
-        // Data is accessible through the DataReader object here.
-
-        sqlConnection1.Close();
-       // }
     }
+}
+
+    
 
 
     
-}
