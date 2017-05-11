3d66eb48f5173ac209da01c30a394154825fb770
mohammdsheikh <p14165162@myemail.dmu.ac.uk>
2017-05-11 21:26:06 +0100

    Data is updating correctly!

--- MyDetails.aspx.cs
+++ MyDetails.aspx.cs
@@ -13,32 +13,18 @@ public partial class MyDetails : System.Web.UI.Page
     {
         if (!IsPostBack)
         {
-            updateChanges();
+            pullData();
         }
-
-
-
-        //Address1.Text =("Ram");
-        //Address2.Text = ("Ram");
-        //Address3.Text = ("Ram");
-        //PostCode.Text = ("Ram");
-        //homePhone.Text = ("Ram");
-        //mobile.Text = ("Ram");
-        //email.Text = ("Ram");
-
     }
 
-    public void updateChanges()
+    public void pullData()
     {
-
-
         if (Session["id"] == null)
         {
             Response.Redirect("LoginPage.aspx");
         }
         else
         {
-
             string addressOne = null;
             int homePhone1 = 0;
             string addressTwo = null;
@@ -50,8 +36,6 @@ public partial class MyDetails : System.Web.UI.Page
 
             try
             {
-
-
                 string sql = "SELECT AddressOne, AddressTwo, AddressThree, HomePhone, PostCode, Mobile, Email FROM Customer where CustomerID =" + Session["id"];
                 //change this connection string... visit www.connectionstrings.com
                 string connString = DBConnection.ConnectionString;
@@ -64,11 +48,6 @@ public partial class MyDetails : System.Web.UI.Page
                         while (j == true)
                         {
                             reader.Read();
-                            // homePhone1 = Convert.ToInt32(reader[0]);
-
-                            //name = reader[0] as string;
-                            // lastName1 = reader[0] as string;
-                            //break for single row or you can continue if you have multiple rows...
                             addressOne = reader["AddressOne"].ToString();
                             addressTwo = reader["AddressTwo"].ToString();
                             addressThree = reader["AddressThree"].ToString();
@@ -76,8 +55,6 @@ public partial class MyDetails : System.Web.UI.Page
                             postCode = reader["PostCode"].ToString();
                             mobile1 = Convert.ToInt32(reader["Mobile"]);
                             email1 = reader["Email"].ToString();
-
-
                             Address1.Text = addressOne;
                             Address2.Text = addressTwo;
                             Address3.Text = addressThree;
@@ -85,10 +62,7 @@ public partial class MyDetails : System.Web.UI.Page
                             PostCode.Text = postCode;
                             mobile.Text = Convert.ToString(mobile1);
                             email.Text = email1;
-
-
                             break;
-
                         }
                     }
                     conn.Close();
@@ -99,44 +73,23 @@ public partial class MyDetails : System.Web.UI.Page
                 // email.Text = ex.ToString();
             }
         }
-        //string address1 = null;
-        //Address1.Text = address1;
-        //string connString;
-        //SqlConnection connection;
-        //connString = DBConnection.ConnectionString;
-        //connection = new SqlConnection(connString);
-        //connection.Open();
-        //SqlCommand command;
-        //SqlDataAdapter adapter = new SqlDataAdapter();
-        //String sql = "";
-        //sql = "Update Customer set AddressOne = @address1 where CustomerID =" + Session["id"];
-        //adapter.UpdateCommand = new SqlCommand(sql, connection);
-        //command = new SqlCommand(sql, connection);
-        //command.Parameters.AddWithValue("@address1", Address1.Text);
-        //adapter.UpdateCommand.ExecuteNonQuery();
-        //command.Dispose();
-        //connection.Close();
-
-
     }
-
 
     protected void Button1_Click(object sender, EventArgs e)
     {
-
         string connString = DBConnection.ConnectionString;
         using (SqlConnection conn = new SqlConnection(connString))
 
         {
-
-            string query = "UPDATE Customer SET AddressOne = @address1  where CustomerID =" + Session["id"];
+            string query = "UPDATE Customer SET AddressOne = @address1, AddressTwo = @address2, AddressThree = @address3, PostCode = @postcode, HomePhone = @homePhone, Mobile = @mobile, Email = @email  where CustomerID =" + Session["id"];
             SqlCommand cmd = new SqlCommand(query, conn);
             cmd.Parameters.AddWithValue("@address1", Address1.Text);
-            //cmd.Parameters.AddWithValue("@Name", txtName.Text);
-            //cmd.Parameters.AddWithValue("@Category", Category.Text);
-            //cmd.Parameters.AddWithValue("@start", txtStart.Text);
-            //cmd.Parameters.AddWithValue("@end", txtend.Text);
-            //cmd.Parameters.AddWithValue("@Description", txtDescription.Text);
+            cmd.Parameters.AddWithValue("@address2", Address2.Text);
+            cmd.Parameters.AddWithValue("@address3", Address3.Text);
+            cmd.Parameters.AddWithValue("@postcode", PostCode.Text);
+            cmd.Parameters.AddWithValue("@homePhone", homePhone.Text);
+            cmd.Parameters.AddWithValue("@mobile", mobile.Text);
+            cmd.Parameters.AddWithValue("@email", email.Text);
             cmd.Connection.Open();
             try
             {
@@ -146,7 +99,6 @@ public partial class MyDetails : System.Web.UI.Page
             {
                 throw new Exception("Error " + ex.Message);
             }
-
         }
     }
 }
