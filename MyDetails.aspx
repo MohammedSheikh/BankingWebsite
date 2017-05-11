0fd9fff1cca70710889d0bf162dea826d6b70cae
mohammdsheikh <p14165162@myemail.dmu.ac.uk>
2017-05-11 21:06:37 +0100

    Fields seem to be updating data correctly (update query)

--- MyDetails.aspx
+++ MyDetails.aspx
@@ -8,24 +8,23 @@
     <h2>Address</h2>
     <p>
 
-        <asp:TextBox OnTextChanged = "textCheck" ID="Address1" runat="server" AutoPostBack="false"></asp:TextBox>
-        <asp:TextBox OnTextChanged = "textCheck2" ID="Address2" runat="server" AutoPostBack="false"></asp:TextBox>
-        <asp:TextBox OnTextChanged = "textCheck3" ID="Address3" runat="server" AutoPostBack="false"></asp:TextBox>
-        <asp:TextBox OnTextChanged = "textCheck4" ID="PostCode" runat="server" AutoPostBack="false"></asp:TextBox>
+        <asp:TextBox  ID="Address1" runat="server" AutoPostBack="false"></asp:TextBox>
+        <asp:TextBox  ID="Address2" runat="server" AutoPostBack="false"></asp:TextBox>
+        <asp:TextBox  ID="Address3" runat="server" AutoPostBack="false"></asp:TextBox>
+        <asp:TextBox  ID="PostCode" runat="server" AutoPostBack="false"></asp:TextBox>
 
     </p>
     <h2>Phone Number</h2>
-    <asp:TextBox OnTextChanged = "textCheck5" ID="homePhone" runat="server" AutoPostBack="false"></asp:TextBox>
-    <asp:TextBox OnTextChanged = "textCheck6" ID ="mobile" runat="server" AutoPostBack="false"></asp:TextBox>
+    <asp:TextBox  ID="homePhone" runat="server" AutoPostBack="false"></asp:TextBox>
+    <asp:TextBox ID ="mobile" runat="server" AutoPostBack="false"></asp:TextBox>
 
     <h2>Email Address</h2>
 
-    <asp:TextBox OnTextChanged = "textCheck7" ID="email" runat="server" AutoPostBack="false"></asp:TextBox>
+    <asp:TextBox  ID="email" runat="server" AutoPostBack="false"></asp:TextBox>
 
     <br />
     <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
     <br />
-    <asp:Button ID="saveChangesButton" runat="server" Text="Save Changes" Enabled="False" Height="35px" />
-
+    
 </asp:Content>
 
--- MyDetails.aspx.cs
+++ MyDetails.aspx.cs
@@ -11,12 +11,32 @@ public partial class MyDetails : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            updateChanges();
+        }
+
+
+
+        //Address1.Text =("Ram");
+        //Address2.Text = ("Ram");
+        //Address3.Text = ("Ram");
+        //PostCode.Text = ("Ram");
+        //homePhone.Text = ("Ram");
+        //mobile.Text = ("Ram");
+        //email.Text = ("Ram");
+
+    }
+
+    public void updateChanges()
+    {
+
 
         if (Session["id"] == null)
         {
             Response.Redirect("LoginPage.aspx");
         }
-        else 
+        else
         {
 
             string addressOne = null;
@@ -44,12 +64,12 @@ public partial class MyDetails : System.Web.UI.Page
                         while (j == true)
                         {
                             reader.Read();
-                           // homePhone1 = Convert.ToInt32(reader[0]);
+                            // homePhone1 = Convert.ToInt32(reader[0]);
 
                             //name = reader[0] as string;
                             // lastName1 = reader[0] as string;
                             //break for single row or you can continue if you have multiple rows...
-                            addressOne = reader["AddressOne"].ToString(); 
+                            addressOne = reader["AddressOne"].ToString();
                             addressTwo = reader["AddressTwo"].ToString();
                             addressThree = reader["AddressThree"].ToString();
                             homePhone1 = Convert.ToInt32(reader["HomePhone"]);
@@ -74,90 +94,59 @@ public partial class MyDetails : System.Web.UI.Page
                     conn.Close();
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-               // email.Text = ex.ToString();
+                // email.Text = ex.ToString();
             }
         }
-        //Address1.Text =("Ram");
-        //Address2.Text = ("Ram");
-        //Address3.Text = ("Ram");
-        //PostCode.Text = ("Ram");
-        //homePhone.Text = ("Ram");
-        //mobile.Text = ("Ram");
-        //email.Text = ("Ram");
-        
-    }
-
-    public void updateChanges()
-    {
-        string address1 = null;
-        Address1.Text = address1;
-        string connString;
-        SqlConnection connection;
-        connString = DBConnection.ConnectionString;
-        connection = new SqlConnection(connString);
-        connection.Open();
-        SqlCommand command;
-        SqlDataAdapter adapter = new SqlDataAdapter();
-        String sql = "";
-        sql = "Update Customer set AddressOne = @address1 where CustomerID =" + Session["id"];
-        adapter.UpdateCommand = new SqlCommand(sql, connection);
-        command = new SqlCommand(sql, connection);
-        command.Parameters.AddWithValue("@address1", Address1.Text);
-        adapter.UpdateCommand.ExecuteNonQuery();
-        command.Dispose();
-        connection.Close();
+        //string address1 = null;
+        //Address1.Text = address1;
+        //string connString;
+        //SqlConnection connection;
+        //connString = DBConnection.ConnectionString;
+        //connection = new SqlConnection(connString);
+        //connection.Open();
+        //SqlCommand command;
+        //SqlDataAdapter adapter = new SqlDataAdapter();
+        //String sql = "";
+        //sql = "Update Customer set AddressOne = @address1 where CustomerID =" + Session["id"];
+        //adapter.UpdateCommand = new SqlCommand(sql, connection);
+        //command = new SqlCommand(sql, connection);
+        //command.Parameters.AddWithValue("@address1", Address1.Text);
+        //adapter.UpdateCommand.ExecuteNonQuery();
+        //command.Dispose();
+        //connection.Close();
 
 
     }
 
-
-
-    protected void textCheck(object sender, EventArgs e)
-    {
-        saveChangesButton.Enabled = !String.IsNullOrEmpty(Address1.Text);
-
-    }
-
-    protected void textCheck2(object sender, EventArgs e)
-    {
-        saveChangesButton.Enabled = !String.IsNullOrEmpty(Address2.Text);
-
-    }
-
-    protected void textCheck3(object sender, EventArgs e)
-    {
-        saveChangesButton.Enabled = !String.IsNullOrEmpty(Address3.Text);
-
-    }
-
-    protected void textCheck4(object sender, EventArgs e)
-    {
-        saveChangesButton.Enabled = !String.IsNullOrEmpty(PostCode.Text);
-
-    }
-
-    protected void textCheck5(object sender, EventArgs e)
-    {
-        saveChangesButton.Enabled = !String.IsNullOrEmpty(homePhone.Text);
-
-    }
-
-    protected void textCheck6(object sender, EventArgs e)
-    {
-        saveChangesButton.Enabled = !String.IsNullOrEmpty(mobile.Text);
-
-    }
-
-    protected void textCheck7(object sender, EventArgs e)
-    {
-        saveChangesButton.Enabled = !String.IsNullOrEmpty(email.Text);
-
-    }
 
     protected void Button1_Click(object sender, EventArgs e)
     {
-        updateChanges();
+
+        string connString = DBConnection.ConnectionString;
+        using (SqlConnection conn = new SqlConnection(connString))
+
+        {
+
+            string query = "UPDATE Customer SET AddressOne = @address1  where CustomerID =" + Session["id"];
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@address1", Address1.Text);
+            //cmd.Parameters.AddWithValue("@Name", txtName.Text);
+            //cmd.Parameters.AddWithValue("@Category", Category.Text);
+            //cmd.Parameters.AddWithValue("@start", txtStart.Text);
+            //cmd.Parameters.AddWithValue("@end", txtend.Text);
+            //cmd.Parameters.AddWithValue("@Description", txtDescription.Text);
+            cmd.Connection.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error " + ex.Message);
+            }
+
+        }
     }
 }
