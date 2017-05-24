8e54616fbfec2093f1bdaadc146b0f5d808acfbc
mohammdsheikh <p14165162@myemail.dmu.ac.uk>
2017-05-24 20:59:08 +0100

    Created and implemented 'ResetPassword' stored procedure into the code

--- ResetPassword.aspx.cs
+++ ResetPassword.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -16,28 +17,36 @@ public partial class ResetPassword : System.Web.UI.Page
     //this button enables users password to be changed
     protected void Button1_Click(object sender, EventArgs e)
     {
-        string connString = DBConnection.ConnectionString;
-        using (SqlConnection conn = new SqlConnection(connString))
+        string strcon = DBConnection.ConnectionString;
+        SqlConnection con = new SqlConnection(strcon);
+        SqlCommand com = new SqlCommand("ResetPassword", con);
+        com.CommandType = System.Data.CommandType.StoredProcedure;
+
+        SqlParameter p1 = new SqlParameter("@password", passwordTextBox.Text);
+        SqlParameter p2 = new SqlParameter("@email", email.Text);
+
+        var returnParameter = com.Parameters.Add("@ReturnVal", SqlDbType.Int);
+        returnParameter.Direction = ParameterDirection.ReturnValue;
+
+        com.Parameters.Add(p1);
+        com.Parameters.Add(p2);
+
+        com.Connection.Open();
+
+        try
         {
-            //SQL Update query
-            string query = "UPDATE Customer SET Password = @password where Email = @email";
-            SqlCommand cmd = new SqlCommand(query, conn);
-            //add param values
-            cmd.Parameters.AddWithValue("@password", passwordTextBox.Text);
-            cmd.Parameters.AddWithValue("@email", email.Text);
-            //open the connection
-            cmd.Connection.Open();
-            try
-            {
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Error " + ex.Message);
-            }
-            lblMessage.Text = "Your password has been updated. You will now be taken to the Login page.";
-            //timer that redirects to login page after 3 seconds
-            Response.AddHeader("REFRESH", "3;URL=LoginPage.aspx");
+            com.ExecuteNonQuery();
         }
+
+        catch (Exception ex)
+        {
+            lblMessage.Text = Convert.ToString(ex);
+            throw new Exception("Error " + ex.Message);
+        }
+
+        lblMessage.Text = "Your password has been updated. You will now be taken to the Login page.";
+        //timer that redirects to login page after 3 seconds
+        Response.AddHeader("REFRESH", "3;URL=LoginPage.aspx");
+            
     }
 }
